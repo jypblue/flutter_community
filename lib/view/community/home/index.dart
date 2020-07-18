3d114ed43b62f3926cd098835a3c7796bf0194be
wangzhaoxiong <wangzhaoxiong@yit.com>
2020-07-18 22:52:03 +0800

    feat: add header

--- lib/view/community/home/index.dart
+++ lib/view/community/home/index.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../../styles/icons.dart';
 import '../../../widgets/mAppbar.dart';
-
-
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -15,19 +15,80 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MAppBar(
-        title: "Home Container Widget",
-        height: 74,
-        ctx: context,
-        header: 'Home'
-      ),
-      body: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(20.0),
-          children: <Widget>[
-
-          ]
-        )
-    );
+        appBar: MAppBar(
+            height: 35,
+            ctx: context,
+            child: Column(children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 77.0,
+                      alignment: Alignment.centerLeft,
+                      child: new Icon(
+                        AppIcons.CAMERA,
+                        size: 15,
+                      ),
+                    ),
+                    Expanded(child: Container()),
+                    Container(
+                      width: 77.0,
+                      alignment: Alignment.center,
+                      height: 35.0,
+                      child: Text('关注',
+                          style: TextStyle(
+                              fontSize: 17, color: Color(0xFF333333))),
+                    ),
+                    Container(
+                      width: 77.0,
+                      alignment: Alignment.center,
+                      height: 35.0,
+                      child: Text('发现',
+                          style: TextStyle(
+                              fontSize: 17, color: Color(0xFF333333))),
+                    ),
+                    Expanded(child: Container()),
+                    new GestureDetector(
+                      onTap: () {
+                        print("Container clicked");
+                      },
+                      child: Container(
+                          alignment: Alignment.centerRight,
+                          width: 77,
+                          height: 24,
+                          margin: EdgeInsets.only(left: 6),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(17.0),
+                              ),
+                              border: Border.all(
+                                  color: Color(0xFFC13B38),
+                                  width: 0.5,
+                                  style: BorderStyle.solid)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              new Icon(
+                                Icons.add,
+                                size: 11,
+                                color: Color(0xFFC13B38),
+                              ),
+                              Text(
+                                '分享图片',
+                                style: TextStyle(
+                                  fontSize: 11,
+                                  color: Color(0xFFC13B38),
+                                ),
+                              )
+                            ],
+                          )),
+                    )
+                  ],
+                ),
+              )
+            ])),
+        body: ListView());
   }
 }
--- lib/widgets/mAppbar.dart
+++ lib/widgets/mAppbar.dart
@@ -6,20 +6,9 @@ import '../styles/colors.dart';
 class MAppBar extends StatefulWidget implements PreferredSizeWidget {
   final double height;
   final Widget child;
-  final String title;
-  final String back;
-  final String header;
   final BuildContext ctx;
 
-  MAppBar(
-      {Key key,
-      this.child,
-      this.title,
-      this.height = 70,
-      this.header = 'Community',
-      this.back = '',
-      this.ctx})
-      : super(key: key);
+  MAppBar({Key key, this.child, this.height = 70, this.ctx}) : super(key: key);
 
   @override
   Size get preferredSize => Size.fromHeight(height);
@@ -39,61 +28,7 @@ class _MAppBarState extends State<MAppBar> {
   Widget build(BuildContext context) {
     return new SafeArea(
       top: true,
-      child: widget.child == null
-          ? new CommunityHeader(title: widget.title, back: widget.back)
-          : widget.child,
-    );
-  }
-}
-
-class CommunityHeader extends StatelessWidget {
-  final String _back;
-  final String _title;
-  const CommunityHeader({
-    Key key,
-    String title,
-    String back,
-  })  : _title = title,
-        _back = back,
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 69.0,
-      padding: EdgeInsets.only(top: 25.0),
-      decoration: BoxDecoration(color: AppColors.white),
-      child: Stack(alignment: Alignment.center, children: <Widget>[
-        Positioned(
-            child: Row(children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  if (Navigator.canPop(context)) {
-                    Navigator.pop(context);
-                  }
-                },
-                child: Container(
-                  width: 52,
-                  height: 44,
-                  padding: EdgeInsets.only(left: 18, right: 6),
-                  alignment: Alignment.center,
-                  child: SvgPicture.asset(AppIcons.ICON_BACK,
-                      width: 15.0, color: Color(0xFF000000)),
-                ),
-              ),
-              Text(
-                this._back,
-                style: TextStyle(fontSize: 16, color: Color(0xFF000000)),
-              )
-            ]),
-            left: 0,
-            top: 0),
-        Container(
-            child: Text(
-          this._title,
-          style: TextStyle(fontSize: 16, color: Color(0xFF000000)),
-        ))
-      ]),
+      child: widget.child,
     );
   }
 }
